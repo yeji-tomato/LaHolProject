db949c3560d06456091b7f7c5dc34bb1f5a67209
kh-b1 <kathychoi1616@gmail.com>
2021-04-09 21:28:40 +0900

    커피클래스 페이지네이션 커밋

---
+++ src/main/java/com/kh/lahol/coffeeclass/page/Pagination.java
@@ -0,0 +1,26 @@
+package com.kh.lahol.coffeeclass.page;
+
+import com.kh.lahol.coffeeclass.model.vo.PageInfo;
+
+public class Pagination {
+
+	public static PageInfo getPageInfo(int currentPage, int listCount) {
+		PageInfo pi = null; 
+		
+		int pageLimit = 5;	  
+		int classLimit = 12; 
+		
+		int maxPage = (int)Math.ceil((double)listCount / classLimit);
+		int startPage = (currentPage - 1) / pageLimit * pageLimit + 1;
+		int endPage = startPage + pageLimit - 1;
+		
+		 
+		if(maxPage < endPage) {
+			endPage = maxPage;
+		}
+		
+		pi = new PageInfo(currentPage,  listCount, pageLimit, maxPage, startPage, endPage, classLimit);
+		
+		return pi;
+	}
+}
