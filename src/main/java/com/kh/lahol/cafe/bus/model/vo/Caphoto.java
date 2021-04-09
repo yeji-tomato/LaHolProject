cecf46a0f5dfe5e4179485f4d3183fafd862d680
Lee-Ye-Ji <dpwl30559@gmail.com>
2021-04-09 10:16:32 +0900

    매장 포장 insert

---
+++ src/main/java/com/kh/lahol/cafe/bus/model/vo/Caphoto.java
@@ -0,0 +1,23 @@
+package com.kh.lahol.cafe.bus.model.vo;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Caphoto {
+	private String caINo;
+	private String caIName;
+	private String caIchName;
+	private String caIPath;
+	private String caIType;
+	private String caIcode;	
+}
--- src/main/java/com/kh/lahol/cafe/user/model/service/CafeServiceImpl.java
+++ src/main/java/com/kh/lahol/cafe/user/model/service/CafeServiceImpl.java
@@ -1,6 +1,25 @@
 package com.kh.lahol.cafe.user.model.service;
 
-public class CafeServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.lahol.cafe.user.model.dao.CafeDao;
+import com.kh.lahol.cafe.user.model.vo.CafeRes;
+
+@Service
+public class CafeServiceImpl implements CafeService{
+	@Autowired
+	private CafeDao caDao;
+	
+	@Override
+	public int hereInsertRes(CafeRes r) {
+		return caDao.hereInsertRes(r);
+	}
+
+	@Override
+	public int togoInsertRes(CafeRes r) {
+		return caDao.togoInsertRes(r);
+	}
 	
 
 }
